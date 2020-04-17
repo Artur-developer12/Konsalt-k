aad177aefa1ba3297c46f127bce0fb831b0ccd39
Artur-developer12 <61634745+Artur-developer12@users.noreply.github.com>
2020-04-18 00:10:42 +0300

    исправление ошибок

--- project/js/main.js
+++ project/js/main.js
@@ -1,15 +1,16 @@
-let btn        = document.querySelectorAll('#btn_modal');
-let modal      = document.querySelector('#modal');
-let modalInner = modal.firstElementChild;
-let modalClose = modalInner.firstElementChild;
-let body       = modal.parentElement;
-let header     = document.querySelector('.header').scrollHeight;
-let nav_inner  = document.querySelector('#nav_inner');
-let links      = document.querySelectorAll('.header_link');
-let burger     = document.querySelector('#burger');
-let header_nav = document.querySelector('.header_nav')
+let btn           = document.querySelectorAll('#btn_modal');
+let modal         = document.querySelector('#modal');
+let modalInner    = modal.firstElementChild;
+let modalClose    = modalInner.firstElementChild;
+let body          = modal.parentElement;
+let nav_inner     = document.querySelector('#nav_inner');
+let links         = document.querySelectorAll('.header_link');
+let burger        = document.querySelector('#burger');
+let header_nav    = document.querySelector('.header_nav');
+let header        = document.querySelector('.header');
+let header_height = header.scrollHeight;
 
-
+// прелоадер
 document.body.onload = function(){
 	setTimeout(function(){
 		let preloader = document.querySelector('#preloader');
@@ -34,19 +35,15 @@ document.body.onload = function(){
  }
 for (let i = 0; i < links.length; i++) {
  	links[i].onclick = () => {
- 		for (let i = 0; i < header_nav.classList.length; i++) {
- 			if (header_nav.classList[i] == 'active') {
+		if (header_nav.classList.contains('active')) {
+ 			header_nav.classList.toggle('active');
+			header_nav.classList.toggle('burger_animate');
+			burger.classList.toggle('click');
 
-		 		header_nav.classList.toggle('active');
-	 			header_nav.classList.toggle('burger_animate');
-	 			burger.classList.toggle('click');
-
-
-		 		if (body.style.overflow == 'hidden') {
-		 			body.style.overflow ='auto';
-				}
- 			}
- 		} 
+	 		if (body.style.overflow == 'hidden') {
+	 			body.style.overflow ='auto';
+			}
+		}
  	}
  } 
   
@@ -67,7 +64,7 @@ for (let link of links) {
 
 // фиксированное меню
 window.onscroll = function(){
-	if (header-10 <= pageYOffset ) {
+	if (header_height-10 <= pageYOffset ) {
 		nav_inner.classList.add('fixed');
 
 	}
@@ -85,11 +82,8 @@ function openModal(){
 		 	modal.classList.toggle('active');
 		 	modalInner.classList.add('modalUp');
 		 	body.style.overflow = 'hidden';
-
-
 		}
 	}
- 
 }
  
  function closeModal(){
@@ -100,24 +94,30 @@ function openModal(){
  	}
  
 	modalClose.onclick = () =>  close ()
+
 		function close (){
 			modalInner.classList.remove('modalUp');
 			modalInner.classList.add('modalDoun');
-
 			setTimeout(function(){
 				modalInner.classList.remove('modalDoun');
 		 		modal.classList.toggle('active');
 				body.style.overflow = 'auto';
-
 			},400)
-
-
 		 }
-	 
  }
 
  openModal();
  closeModal();
+
+ // паралакс
+document.querySelector('.intro').addEventListener('mousemove', function(e){
+	let x = e.pageX / this.offsetWidth;
+	let y = e.pageY / this.offsetHeight;
+
+	document.querySelector('.intro_img').style.transform = `translate(-${x*30}px, -${y*30}px)`;
+	document.querySelector('.intro_img').style.transition = '0.6s ease-out';
+});
+
 
  
 
